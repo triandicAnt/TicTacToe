d511efeef876febdcf796659b37e22a50792e384
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-10-21 17:16:56 -0400

    tic tac toe

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,5 @@
 from random import randint
+import random
 
 # Create Board
 board = range(0,9)
@@ -27,19 +28,113 @@ print '************************************************************'
 print
 
 yours = ''
-comp = ''
+comps = ''
+
 if guessInLower == 'x':
 	yours = 'x'
-	comp = 'o'
+	comps = 'o'
 else:
 	yours = 'o'
-	comp = 'x'
+	comps = 'x'
+
 print '************************************************************'
 print
-print 'Yours is \'',yours, '\' and Computer\'s is', " \'", comp,'\'' 
+print 'Yours is \'',yours, '\' and Computer\'s is', " \'", comps,'\'' 
 print
 print '************************************************************'
 print
+
 # Display board
+
 show()
 print
+
+#check for winner
+
+def checkline(char, spot1, spot2, spot3):
+
+	if board[spot1] == char and board[spot2] == char and board[spot3] == char:
+		return True
+
+def checkWinner(char):
+
+	if checkline(char, 0,1,2):
+		return True
+	if checkline(char, 3,4,5):
+		return True
+	if checkline(char, 6,7,8):
+		return True
+	if checkline(char, 0,3,6):
+		return True
+	if checkline(char, 1,4,7):
+		return True
+	if checkline(char, 2,5,8):
+		return True
+	if checkline(char, 0,4,8):
+		return True
+	if checkline(char, 2,4,6):
+		return True
+
+# Start Game
+
+while True:
+	input = raw_input('Select a spot: ')
+	input = int(input)
+
+	if board[input] != yours and board[input] != comps:
+		board[input] = yours
+
+
+		# check for winner
+
+		if checkWinner(yours) == True:
+			print
+			print '~~~~~ You Win!    ~~~~~'
+			print '~~~~~ Game Over!  ~~~~~'
+			print
+			show()
+			print
+
+			break
+
+		# computer's turn
+		# generate a random number between 0 and 8
+		# fill the spot for that random number
+
+		compWins = False
+
+		while True:
+			random.seed()
+			opponent = random.randint(0,8)
+
+			# fill the spot
+			if board[opponent] != yours and board[opponent] != comps:
+				board[opponent] = comps
+
+				# check for winner
+
+				if checkWinner(comps) == True:
+					print
+					print '~~~~~ Computer Wins! You are a loser!! ;) ~~~~~'
+					print '~~~~~ Game Over! ~~~~~'
+					print
+					compWins = True
+					break
+
+				break
+		
+		if compWins == True:
+			show()
+			print
+			break
+
+	else:
+		print 'This spot is occupied! Select another: '
+
+	print
+
+	# display board
+	show()
+	print
+
+
